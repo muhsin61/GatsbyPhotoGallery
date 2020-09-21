5bc5a5247625ebab971b3342c369e681dca995af
Muhsin Köse <36308247+muhsin61@users.noreply.github.com>
2020-09-21 19:42:44 +0300

    big image

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,9 @@ const IndexPage = ({ data }) => {
   const [input,senInput] = useState("")
   const [show,setShow] = useState(false)
 
+  const [bigImg,setBigImg] = useState(false)
+  const [bigImgSrc,setBigImgSrc] = useState("")
+
   let addNewPhoto = (e) => {
     const file = e.target.files[0];
     const toBase64 = file => new Promise((resolve, reject) => {
@@ -37,6 +40,10 @@ const IndexPage = ({ data }) => {
       }
     })
   }
+  let showPhoto = (imgSrc) => {
+    setBigImgSrc(imgSrc.src)
+    setBigImg(true)
+  }
   return (
   <div>
     
@@ -49,14 +56,19 @@ const IndexPage = ({ data }) => {
       </div>
       
     <h1 >Fotoğraf galerim</h1>
-    <p className="showNewPhoto" onClick={()=>setShow(true)}>Yeni Fotoğraf Ekle</p>
+    <p className="showNewPhoto" onClick={()=>setShow(true)} >Yeni Fotoğraf Ekle</p>
 
     <div className="photos">
       {data.allFile.edges.map(({ node }) => {
-        return <Img className="img" key={node.id} fluid={node.childImageSharp.fluid} />
+        return <div key={node.id} className="img" onClick={()=>showPhoto(node.childImageSharp.fluid)}><Img className="img2"  fluid={node.childImageSharp.fluid} /></div>
       })}
     </div>
-
+    {bigImg ? (<div className="bigImg">
+      <div className="imgBack" onClick={()=>setBigImg(false)}></div>
+      <img src={bigImgSrc} alt="there is  no img" />
+    </div>
+) : null}
+    
     <footer>
       <p>Kodlara <a href="https://github.com/muhsin61/GatsbyPhotoGallery">Github</a> üzerinden ulaşabilrisiniz.</p>
     </footer>
